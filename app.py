08ebddf13f18e7fadb0e1849809a64305527248c
Pior Bastida <pior@pbastida.net>
2016-06-17 16:46:04 -0400

    Initial test on CloudFoundry

---
+++ app.py
@@ -0,0 +1,17 @@
+"""Cloud Foundry test"""
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+# On Bluemix, get the port number from the environment variable VCAP_APP_PORT
+# When running this app on the local machine, default the port to 8080
+port = int(os.getenv('VCAP_APP_PORT', 8080))
+
+
+@app.route('/')
+def home():
+    return 'Hello World! Aqualove is running on port ' + str(port)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
